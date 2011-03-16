5d1df8e194b74123715284b89eb9bf2a66c55315
Michael Bayne <mdb@samskivert.com>
2011-03-16 12:27:02 -0700

    Stub client implementation.

---
+++ core/src/main/java/com/samskivert/nexus/client/NexusClient.java
@@ -0,0 +1,15 @@
+//
+// $Id$
+//
+// Nexus Core - a framework for developing distributed applications
+// http://github.com/samskivert/nexus/blob/master/LICENSE
+
+package com.samskivert.nexus.client;
+
+/**
+ * Manages connections to Nexus servers. Provides access to distributed objects and services.
+ */
+public class NexusClient
+{
+    // TODO!
+}
