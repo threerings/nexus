fd76cb6feda9a6ded548ee5e3eebd36d29598d15
Michael Bayne <mdb@samskivert.com>
2011-03-16 12:27:29 -0700

    Marker interface for Nexus services.

---
+++ core/src/main/java/com/samskivert/nexus/distrib/NexusService.java
@@ -0,0 +1,14 @@
+//
+// $Id$
+//
+// Nexus Core - a framework for developing distributed applications
+// http://github.com/samskivert/nexus/blob/master/LICENSE
+
+package com.samskivert.nexus.distrib;
+
+/**
+ * A marker interface identifying a Nexus distributed service.
+ */
+public interface NexusService
+{
+}
