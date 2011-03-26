a6917e92823a63343bd1510e85318f14433e8a3e
Michael Bayne <mdb@samskivert.com>
2011-03-25 20:28:27 -0700

    Automatically catch NexusException (and other failures) when processing a service call and communicate said failures back to the client (if possible).

--- core/src/main/java/com/samskivert/nexus/distrib/DistribUtil.java
+++ core/src/main/java/com/samskivert/nexus/distrib/DistribUtil.java
@@ -6,6 +6,10 @@
 
 package com.samskivert.nexus.distrib;
 
+import com.samskivert.nexus.util.Callback;
+
+import static com.samskivert.nexus.util.Log.log;
+
 /**
  * Provides access to interfaces that should not be called by normal clients, but which must be
  * accessible to the Nexus implementation code, which may reside in a different package.
@@ -24,7 +28,25 @@ public class DistribUtil
 
     public static void dispatchCall (NexusObject object, int attrIdx, short methId, Object[] args)
     {
-        ((DService.Dispatcher)object.getAttribute(attrIdx)).dispatchCall(methId, args);
+        Object cb = args[args.length-1];
+        DService.Dispatcher disp = null;
+        try {
+            disp = (DService.Dispatcher)object.getAttribute(attrIdx);
+            disp.dispatchCall(methId, args);
+
+        } catch (NexusException ne) {
+            if (cb instanceof Callback<?>) {
+                ((Callback<?>)cb).onFailure(ne);
+            } else {
+                log.warning("Service call failed", "obj", object, "attr", disp, "methId", methId, ne);
+            }
+
+        } catch (Throwable t) {
+            log.warning("Service call failed", "obj", object, "attr", disp, "methId", methId, t);
+            if (cb instanceof Callback<?>) {
+                ((Callback<?>)cb).onFailure(new NexusException("Internal error.")); // TODO: i18n
+            }
+        }
     }
 
     private DistribUtil () {} // no constructsky
