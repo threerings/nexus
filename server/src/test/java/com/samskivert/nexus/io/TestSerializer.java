7dc2f2af062fc7a94fd0e8c062132f8f42cf090a
Michael Bayne <mdb@samskivert.com>
2011-03-27 14:06:19 -0700

    Added a test for ServerInput, using output generated by ClientOutputTest.

---
+++ server/src/test/java/com/samskivert/nexus/io/TestSerializer.java
@@ -0,0 +1,15 @@
+//
+// $Id$
+
+package com.samskivert.nexus.io;
+
+/**
+ * A serializer that handles our test classes.
+ */
+public class TestSerializer extends AbstractSerializer
+{
+    public TestSerializer ()
+    {
+        // presently nothing to register
+    }
+}
