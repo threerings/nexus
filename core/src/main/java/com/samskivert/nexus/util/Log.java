3134e0af3c97c6db78a1d257458cc6d08b62ea3f
Michael Bayne <mdb@samskivert.com>
2011-03-28 20:51:34 -0700

    Some log revamping to make it easier to wire GWT logging into place.

--- core/src/main/java/com/samskivert/nexus/util/Log.java
+++ core/src/main/java/com/samskivert/nexus/util/Log.java
@@ -12,52 +12,49 @@ package com.samskivert.nexus.util;
 public class Log
 {
     /** A level of indirection that allows us to plug in Java or GWT-based logging. */
-    public interface Logger {
+    public static abstract class Logger {
         /** Logs an info level message, with the supplied arguments and optional Throwable cause in
          * final position. For example:
          * {@code log.info("Thing happened", "info", data, "also", moredata);} */
-        void info (String message, Object... args);
+        public abstract void info (String message, Object... args);
 
         /** Logs a warning level message, with the supplied arguments and optional Throwable cause
          * in final position. For example:
          * {@code log.info("Bad thing happened", "info", data, cause);} */
-        void warning (String message, Object... args);
-
-        /** Implementation detail that's easier to expose here; please ignore. */
-        void log (Object level, String message, Throwable cause);
+        public abstract void warning (String message, Object... args);
 
         /** Disables info logging, shows only warnings and above. */
-        void setWarnOnly ();
+        public abstract void setWarnOnly ();
+
+        protected void format (Object level, String message, Object... args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.append(message);
+            if (args.length > 1) {
+                sb.append(" [");
+                for (int ii = 0, ll = args.length/2; ii < ll; ii++) {
+                    if (ii > 0) {
+                        sb.append(", ");
+                    }
+                    sb.append(args[2*ii]).append("=").append(args[2*ii+1]);
+                }
+                sb.append("]");
+            }
+            Object last = (args.length % 2 == 1) ? args[args.length-1] : null;
+            Throwable cause = (last instanceof Throwable) ? (Throwable)last : null;
+            log(level, sb.toString(), cause);
+        }
+
+        /** Called with a formatted log message and (possibly null) cause. */
+        protected abstract void log (Object level, String message, Throwable cause);
     }
 
-    // TODO: some secret static initialization that wires up either a Java logger or GWT depending
-    // on where we're running
-
     /** Dispatch log messages through this instance. */
-    public static final Logger log = new JavaLogger("nexus");
+    public static Logger log = new JavaLogger("nexus");
 
     private Log () {} // no constructsky
 
-    protected static void format (Logger logger, Object level, String message, Object... args)
-    {
-        StringBuilder sb = new StringBuilder();
-        sb.append(message);
-        if (args.length > 1) {
-            sb.append(" [");
-            for (int ii = 0, ll = args.length/2; ii < ll; ii++) {
-                if (ii > 0) {
-                    sb.append(", ");
-                }
-                sb.append(args[2*ii]).append("=").append(args[2*ii+1]);
-            }
-            sb.append("]");
-        }
-        Object last = (args.length % 2 == 1) ? args[args.length-1] : null;
-        Throwable cause = (last instanceof Throwable) ? (Throwable)last : null;
-        logger.log(level, sb.toString(), cause);
-    }
-
-    protected static class JavaLogger implements Logger
+    protected static class JavaLogger extends Logger
     {
         public JavaLogger (String name) {
             _impl = java.util.logging.Logger.getLogger(name);
@@ -65,22 +62,22 @@ public class Log
 
         public void info (String message, Object... args) {
             if (_impl.isLoggable(java.util.logging.Level.INFO)) {
-                format(this, java.util.logging.Level.INFO, message, args);
+                format(java.util.logging.Level.INFO, message, args);
             }
         }
 
         public void warning (String message, Object... args) {
             if (_impl.isLoggable(java.util.logging.Level.WARNING)) {
-                format(this, java.util.logging.Level.WARNING, message, args);
+                format(java.util.logging.Level.WARNING, message, args);
             }
-        }
-
-        public void log (Object level, String message, Throwable cause) {
-            _impl.log((java.util.logging.Level)level, message, cause);
         }
 
         public void setWarnOnly () {
             _impl.setLevel(java.util.logging.Level.WARNING);
+        }
+
+        protected void log (Object level, String message, Throwable cause) {
+            _impl.log((java.util.logging.Level)level, message, cause);
         }
 
         protected final java.util.logging.Logger _impl;
