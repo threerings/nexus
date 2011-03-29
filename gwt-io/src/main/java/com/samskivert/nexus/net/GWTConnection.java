76c3f4603a465a6c3ac45d0b35a83e81380b7ec9
Michael Bayne <mdb@samskivert.com>
2011-03-28 21:05:11 -0700

    For some reason, storing the WebSocket reference in "this" was not working, so we pass it back in onOpen() and pass it in to wsSend() and wsClose().

--- gwt-io/src/main/java/com/samskivert/nexus/net/GWTConnection.java
+++ gwt-io/src/main/java/com/samskivert/nexus/net/GWTConnection.java
@@ -6,15 +6,21 @@
 
 package com.samskivert.nexus.net;
 
+import com.google.gwt.core.client.JavaScriptObject;
 import com.samskivert.nexus.distrib.NexusException;
 import com.samskivert.nexus.io.GWTIO;
 import com.samskivert.nexus.util.Callback;
+
+import static com.samskivert.nexus.util.Log.log;
 
 /**
  * Implements a Nexus connection using WebSockets.
  */
 public class GWTConnection extends Connection
 {
+    /** The (root-relative) path at which we will make our WebSocket connections. */
+    public static final String WS_PATH = "nexusws";
+
     /**
      * Creates an instance which will initiate a Nexus protocol connection with the specified host.
      * @param callback will be notified on connection completion, or failure.
@@ -24,13 +30,13 @@ public class GWTConnection extends Connection
         super(host);
         _szer = szer;
         _callback = callback;
-        wsConnect("ws://" + host + ":" + port + "/nexusws");
+        wsConnect("ws://" + host + ":" + port + "/" + WS_PATH);
     }
 
     @Override // from Connection
     public void close ()
     {
-        wsClose();
+        wsClose(_ws);
     }
 
     @Override // from Connection
@@ -38,7 +44,7 @@ public class GWTConnection extends Connection
     {
         StringBuffer payload = new StringBuffer();
         GWTIO.newOutput(_szer, payload).writeValue(request);
-        wsSend(payload.toString());
+        wsSend(_ws, payload.toString());
     }
 
     @Override // from Connection
@@ -48,8 +54,9 @@ public class GWTConnection extends Connection
         run.run();
     }
 
-    protected void onOpen ()
+    protected void onOpen (JavaScriptObject ws)
     {
+        _ws = ws;
         _callback.onSuccess(this);
         _callback = null; // note that we successfully connected
     }
@@ -76,53 +83,44 @@ public class GWTConnection extends Connection
     }
 
     protected native void wsConnect (String url) /*-{
-        var that = this;
         if (!$wnd.WebSocket) {
-             that.@com.samskivert.nexus.net.GWTConnection::onError(Ljava/lang/String;)(
+             this.@com.samskivert.nexus.net.GWTConnection::onError(Ljava/lang/String;)(
                  "WebSocket not supported by this browser.");
             return;
         }
-        console.log("WebSocket connecting to " + url);
-        that._ws = new $wnd.WebSocket(url);
-        console.log("WebSocket connected " + that._ws.readyState);
+        this._ws = new $wnd.WebSocket(url);
 
-        that._ws.onopen = function (event) {
-            console.log("WebSocket["+url+"].onopen: " + event);
-            that.@com.samskivert.nexus.net.GWTConnection::onOpen();
+        var conn = this;
+        this._ws.onopen = function (event) {
+            conn.@com.samskivert.nexus.net.GWTConnection::onOpen(Lcom/google/gwt/core/client/JavaScriptObject;)(conn._ws);
         };
 
-        that._ws.onmessage = function (event) {
-            console.log("WebSocket["+url+"].onmessage: " + event);
+        this._ws.onmessage = function (event) {
             if (event.data) {
-                that.@com.samskivert.nexus.net.GWTConnection::onMessage(Ljava/lang/String;)(
+                conn.@com.samskivert.nexus.net.GWTConnection::onMessage(Ljava/lang/String;)(
                     event.data);
             }
         };
 
-        that._ws.onerror = function (event) {
-             console.log("WebSocket["+url+"].onerror: " + event);
-             that.@com.samskivert.nexus.net.GWTConnection::onError(Ljava/lang/String;)(
+        this._ws.onerror = function (event) {
+             conn.@com.samskivert.nexus.net.GWTConnection::onError(Ljava/lang/String;)(
                  "TODO");
         };
 
-        that._ws.onclose = function (event) {
-             console.log("WebSocket["+url+"].onclose: " + event);
-             that.@com.samskivert.nexus.net.GWTConnection::onClose()();
+        this._ws.onclose = function (event) {
+             conn.@com.samskivert.nexus.net.GWTConnection::onClose()();
         };
     }-*/;
 
-    protected native void wsSend (String message) /*-{
-        if (this._ws) {
-            this._ws.send(message);
-        }
+    protected native void wsSend (JavaScriptObject ws, String message) /*-{
+        ws.send(message);
     }-*/;
 
-    protected native void wsClose () /*-{
-        if (this._ws) {
-            this._ws.close();
-        }
+    protected native void wsClose (JavaScriptObject ws) /*-{
+        ws.close();
     }-*/;
 
     protected final GWTIO.Serializer _szer;
     protected Callback<Connection> _callback;
+    protected JavaScriptObject _ws;
 }
