673177422c2922fa5ea1bdb77df5b8536e48cacb
Michael Bayne <mdb@samskivert.com>
2013-04-21 14:36:46 -0700

    This should be an abstract class (like Action).
    We definitely do not want you to try to do something clever like implement
    Request on a class that does something else. A Request is meant to be a
    closure, and nothing else.

--- server/src/main/java/com/threerings/nexus/distrib/Request.java
+++ server/src/main/java/com/threerings/nexus/distrib/Request.java
@@ -8,10 +8,10 @@ package com.threerings.nexus.distrib;
  * An RPC request invoked in the context of a Nexus entity. The sender is blocked until the request
  * is processed, and the return value can be delivered to the sender.
  */
-public interface Request<E,R>
+public abstract class Request<E,R>
 {
     /**
      * Called with the requested entity on the server and thread appropriate for said entity.
      */
-    R invoke (E entity);
+    public abstract R invoke (E entity);
 }
